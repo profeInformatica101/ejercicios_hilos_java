da00165dc8799151e89c470cc330b78aacdd4bb0
Pablo <jdommay863@g.educaand.es>
2025-11-07 20:04:52 +0100

    Estado nuevo

--- src/com/LanzadorHilosNew.java
+++ src/com/LanzadorHilosNew.java
@@ -2,10 +2,8 @@ package com;
 
 public class LanzadorHilosNew {
 	public static void main(String [] args) throws InterruptedException{
-		Thread h = new Thread(new Hilo("runneable"));
-		h.start();
+		Thread h = new Thread(new Hilo("nuevo"));
 		System.out.println(h.getName() + "---> "+ h.getState());
-		Thread.sleep(50);
-		
+
 	}
 }
