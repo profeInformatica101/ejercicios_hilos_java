330c37ebe79964095dec1a55e61245c476b84df1
silviamg9 <moreno.garcia.silvia.maria@iescamas.es>
2025-11-07 20:10:47 +0100

    subir clase Hilo y clase LanzadorHilosNew

--- src/com/LanzadorHilosNew.java
+++ src/com/LanzadorHilosNew.java
@@ -2,14 +2,29 @@ package com;
 
 public class LanzadorHilosNew {
 
+	private static final Object LOCK = new Object();
+	
 	public static void main(String[] args) throws InterruptedException{
 		// TODO Auto-generated method stub
 
-		Thread h = new Thread(new Hilo("runneable"));
-		h.start();
-	 
-		System.out.println(h.getName() + " ---> " + h.getState());
-		Thread.sleep(50);	
+		Thread t1 = new Thread(() -> {
+            synchronized (LOCK) {
+                try { Thread.sleep(2000); } catch (InterruptedException ignored) {}
+            }
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+            synchronized (LOCK) {
+                System.out.println("Bloqueado: " + Thread.currentThread().getName());
+            }
+        }, "t2");
+
+        t1.start();
+        Thread.sleep(100);
+        t2.start();
+        Thread.sleep(50);
+        System.out.println(t2.getName() + " -> " + t2.getState());
+	
 		
 	}
 
